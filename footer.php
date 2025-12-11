c07d78e4a000478c5b8f55c8d42144f32183f9e7
spcole88 <scott.cole88@gmail.com>
2025-12-11 14:33:44 +0000

    Added footer content and some footer styling.

--- footer.php
+++ footer.php
@@ -1,12 +1,8 @@
 </main>
 <footer class="scst-footer grid-general">
     <div class="scst-footer__contact">
-
-    </div>
-    <img class="scst-footer__logo" src="" alt="">
-    <div class="scst-footer__footnote">
-
-    </div>
+        <a href="mailto:scott.cole88@gmail.com">scott.cole88@gmail.com</a>
+</div>
 </footer>
 
 <?php wp_footer();?>
